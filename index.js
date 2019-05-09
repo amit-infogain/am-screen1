38d9f6412936a40f6b4559505653f46ee4d53a56
amit-infogain <amit.tiwari@infogain.com>
2019-05-09 15:22:14 +0530

    3 files modified, 1 added

--- index.js
+++ index.js
@@ -1,33 +1,8 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Login from './login';
 import './style.scss';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      name: 'React'
-    };
-  }
-
-  render() {
-    return (
-      <div id="sm_login">
-        <div class="wrapper">
-          <div class="left_half">
-              <div class="inner_wrap">
-                  <div class="logo">Logo</div>
-              </div>
-          </div>
-          <div class="right_half">
-              <div class="inner_wrap">
-                <img src="./img/illust.svg" alt="" />
-              </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
-}
-
-render(<App />, document.getElementById('root'));
+ReactDOM.render(<Login />, document.querySelector('#root'));
+    
