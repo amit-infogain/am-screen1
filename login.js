a566b04bee8300ea7751b2a9cee9a6733a024a8e
amit-infogain <amit.tiwari@infogain.com>
2019-05-10 11:25:19 +0530

    3 files modified

--- login.js
+++ login.js
@@ -10,23 +10,23 @@ class TextFields extends React.Component {
 
     return (
 
-      <div id="sm_login">
-        <div className="wrapper">
-          <div className="left_half">
+      <div id="sm_login" className="container">
+        <div className="wrapper row">
+          <div className="row left_half">
             <div className="inner_wrap">
               <div className="logo">Logo</div>
               <form noValidate autoComplete="off">
                 <TextField
                   id="standard-name"
                   placeholder="ID"
-                  margin="normal"
+                  margin="none"
                   fullWidth
                   className="custom_inp"
                 />
                 <TextField
                   id="standard-name"
                   placeholder="Password"
-                  margin="normal"
+                  margin="none"
                   fullWidth
                   className="custom_inp"
                 />
@@ -34,9 +34,10 @@ class TextFields extends React.Component {
                   Sign In
                 </Button>
               </form>
+                
             </div>
           </div>
-          <div className="right_half">
+          <div className="row right_half">
             <div className="inner_wrap">
               <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/Johan_Christian_Dahl_-_Dresden_by_Moonlight_-_Google_Art_Project.jpg/640px-Johan_Christian_Dahl_-_Dresden_by_Moonlight_-_Google_Art_Project.jpg" className="img-responsive" alt="" />
             </div>
