6876fee05f53c10ec755bc9b97a759154b21a6ba
amit-infogain <amit.tiwari@infogain.com>
2019-05-13 12:09:51 +0530

    3 files modified

--- home_first.js
+++ home_first.js
@@ -1,65 +1,54 @@
 import React from 'react';
 import TextField from '@material-ui/core/TextField';
-import './login.scss';
+import './home_first.scss';
 import Button from '@material-ui/core/Button';
 import Checkbox from '@material-ui/core/Checkbox';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 
 
-class Login extends React.Component {
+class HomeFirst extends React.Component {
 
   render() {
 
     return (
 
-      <div id="sm_login" className="container">
+       <div id="sm_home_first_page" className="container">
         <div className="wrapper row">
-          <div className="flex left_half">
+
+          <div class="sidebar">
+            <HomeIcon className="iconStyle1" />
+            <Avatar alt="Remy Sharp" src="https://cdn1.iconfinder.com/data/icons/user-pictures/101/malecostume-512.png" className="avatarBottom" />
+          </div>
+
+        <div class="main">
+          <div className="left_col flex">
             <div className="inner_wrap">
-              <div className="logo">Logo</div>
-              <form noValidate autoComplete="off">
-                <TextField
-                  id="standard-name"
-                  placeholder="ID"
-                  margin="none"
-                  fullWidth
-                  className="custom_inp"
-                />
-                <TextField
-                  id="standard-name"
-                  placeholder="Password"
-                  margin="none"
-                  fullWidth
-                  className="custom_inp"
-                />
-                <Button variant="contained" className="defButton" fullWidth >
-                  Sign In
-                </Button>
-              </form>
-              <div className="row checkMark">
-                <FormControlLabel
-                  control={
-                    <Checkbox
-                      value="checkedA"
-                    />
-                  }
-                  label="Save ID"
-                />
-
-                <Button className="clearButton" >
-                  Find Password
-              </Button>
-
-              </div>
+                <h2>Welcome to Your Workspace! </h2>
+                <p> All the members in workspace can use 
+a variety of features to collaborate tightly. </p>
+                <ul>
+                  <li>Multiple channels in one workspace</li>
+                  <li>Reply to a message</li>
+                  <li>Catch up important information on the right area</li>
+                  <li>Understand history with longer storage period</li>
+                </ul>
+                  <Button variant="contained" className="cst_button1" size="large" >
+      Create Workspace
+                  <AddIcon />
+                  </Button>
             </div>
           </div>
-          <div className="flex right_half">
+
+          <div className="right_col flex">
             <div className="inner_wrap">
               <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/Johan_Christian_Dahl_-_Dresden_by_Moonlight_-_Google_Art_Project.jpg/640px-Johan_Christian_Dahl_-_Dresden_by_Moonlight_-_Google_Art_Project.jpg" className="img-responsive" alt="" />
             </div>
           </div>
+          </div>
+
         </div>
       </div>
+
     );
   }
 }
--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Login from './login';
+import HomeFirst from ''
 import './style.scss';
 
 ReactDOM.render(<Login />, document.querySelector('#root'));
