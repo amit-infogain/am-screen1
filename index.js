1e308245ea00e496d53d1b5110cba124ea85f19b
amit-infogain <amit.tiwari@infogain.com>
2019-05-13 13:47:51 +0530

    2 files modified

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Login from './login';
 import HomeFirst from './home_first'
+import WSView from './ws_view';
 import './style.scss';
 
-ReactDOM.render(<HomeFirst />, document.querySelector('#root'));
+ReactDOM.render(<WSView />, document.querySelector('#root'));
     
