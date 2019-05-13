28815c3bcfd338acfcf5ffe6d9c4d04158c9a234
amit-infogain <amit.tiwari@infogain.com>
2019-05-13 18:35:39 +0530

    1 file modified

--- ws_view.js
+++ ws_view.js
@@ -5,6 +5,12 @@ import AddIcon from '@material-ui/icons/Add';
 import HomeIcon from '@material-ui/icons/Home';
 import Avatar from '@material-ui/core/Avatar';
 import './ws_view.scss';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
+import Typography from '@material-ui/core/Typography';
 
 class WSView extends React.Component {
 
@@ -25,10 +31,53 @@ class WSView extends React.Component {
               <div class="header">
                 <h2>Mentions Me 3</h2>
               </div>
-              <div class="list">
-                <Avatar alt="Remy Sharp" src="https://cdn1.iconfinder.com/data/icons/user-pictures/101/malecostume-512.png" />
-
-              </div>
+               <List >
+                  <ListItem alignItems="flex-start">
+                    <ListItemAvatar>
+                      <Avatar alt="Remy Sharp" src="https://cdn1.iconfinder.com/data/icons/user-pictures/101/malecostume-512.png" />
+                    </ListItemAvatar>
+                    <ListItemText
+                      primary="Mulien Joe"
+                      secondary={
+                        <React.Fragment>
+                          {"@Karen Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard."}
+                        </React.Fragment>
+                      }
+                    />
+                  </ListItem>
+                  <ListItem alignItems="flex-start">
+                    <ListItemAvatar>
+                      <Avatar alt="Remy Sharp" src="https://cdn1.iconfinder.com/data/icons/user-pictures/101/malecostume-512.png" />
+                    </ListItemAvatar>
+                    <ListItemText
+                      primary="Summer BBQ"
+                      secondary={
+                        <React.Fragment>
+                          <Typography component="span" color="textPrimary">
+                            to Scott, Alex, Jennifer
+              </Typography>
+                          {" — Wish I could come, but I'm out of town this…"}
+                        </React.Fragment>
+                      }
+                    />
+                  </ListItem>
+                  <ListItem alignItems="flex-start">
+                    <ListItemAvatar>
+                      <Avatar alt="Remy Sharp" src="https://cdn1.iconfinder.com/data/icons/user-pictures/101/malecostume-512.png" />
+                    </ListItemAvatar>
+                    <ListItemText
+                      primary="Oui Oui"
+                      secondary={
+                        <React.Fragment>
+                          <Typography component="span" color="textPrimary">
+                            Sandra Adams
+              </Typography>
+                          {' — Do you have Paris recommendations? Have you ever…'}
+                        </React.Fragment>
+                      }
+                    />
+                  </ListItem>
+                </List>
             </div>
 
             <div className="col right">
